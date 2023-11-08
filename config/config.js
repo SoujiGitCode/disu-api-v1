bd474b4dc640511f194a65da4e2d5ec6bfcca9e2
SoujiGitCode <dev.reinaldo.cardenas@gmail.com>
2023-11-08 18:00:09 +0100

    new env config file

---
+++ config/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || 'root',
+        database: process.env.DB_DATABASE || 'disu-app-v1-test',
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 3306,
+        dialect: process.env.DB_DIALECT || 'mysql',
+        logging: false, // Set to console.log to see the raw SQL queries
+        define: {
+            timestamps: true,
+            underscored: true,
+        },
+
+    },
+
+};
