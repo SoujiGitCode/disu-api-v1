14eeeb434ea5bd385ded0bc10b46863ea000c784
SoujiGitCode <dev.reinaldo.cardenas@gmail.com>
2023-12-30 02:01:07 +0100

    sleep is back only 3 secs

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,9 +11,9 @@ const emailService = require('../services/emailService');
 
 
 console.log(process.env.SENDGRID_API_KEY)
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 router.post('/create', upload.none(), async (req, res) => {
     try {
@@ -50,6 +50,9 @@ router.post('/create', upload.none(), async (req, res) => {
         try {
             await emailService.sendWelcomeEmail(newUser);
             console.log('Correo de bienvenida enviado a:', newUser.email);
+
+            // Espera de 3 segundos antes de enviar el siguiente correo
+            await sleep(3);
 
             await emailService.sendQRCodeEmail(newUser);
             console.log('Correo con código QR enviado a:', newUser.email);
